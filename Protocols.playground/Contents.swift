730810b080349bff1ccf3955f2b54007895f51c6
Luiz Carlos <luizgadao@gmail.com>
2015-08-11 21:55:43 -0300

    behavior with protocols

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -1,4 +1,5 @@
 //: Playground - noun: a place where people can play
+import Foundation
 
 protocol FullyNamed{
     var fullName:String { get }
@@ -21,3 +22,46 @@ struct Friend: FullyNamed{
 
 let friend = Friend(firstName: "Luiz Carlos", middleName: "Gonçalves", lastName: "dos Anjos")
 friend.fullName
+
+
+protocol Payable{
+    func pay()-> (basePay:Int, benefits:Int, deductions:Int, vacationTime:Int)
+}
+
+class Employee{
+    let name:String
+    let address:String
+    let startDate:NSDate
+    let type:String
+    
+    var department:String?
+    var reportsT:Employee?
+    
+    init(fullName:String, employeeAddress:String, employeeStartDate:NSDate, employeeType:String){
+        name = fullName
+        address = employeeAddress
+        startDate = employeeStartDate
+        type = employeeType
+    }
+}
+
+class HourlyEmployee:Employee, Payable{
+    let hourlyWage = 12
+    let hourWorked = 40
+    let availableVacation = 0
+    
+    func pay() -> (basePay: Int, benefits: Int, deductions: Int, vacationTime: Int) {
+        return ( (hourlyWage * hourWorked), 0, 0, availableVacation )
+    }
+}
+
+class SalariedEmployee:Employee{
+    let salary = 40909
+}
+
+
+
+
+
+
+
